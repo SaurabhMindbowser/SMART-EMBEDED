1355e3a93b3fc616a235f9b1eecaf4e880e7efd8
Saurabh Nikam <saurabh.nikam@mindbowser.com>
2025-04-26 17:55:54 +0000

    q

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 console.log("SMART App initializing...");
 
-// Check if we are coming back from OAuth2 login
 if (window.location.href.includes('code=') || window.location.href.includes('state=')) {
     console.log('Detected OAuth2 callback...');
 
@@ -9,7 +8,6 @@ if (window.location.href.includes('code=') || window.location.href.includes('sta
             console.log('FHIR Client Ready:', client);
 
             document.getElementById('status').textContent = 'Fetching Patient Details...';
-
             console.log('Access Token:', client.state.tokenResponse.access_token);
             console.log('Patient ID:', client.patient.id);
 
@@ -25,7 +23,7 @@ if (window.location.href.includes('code=') || window.location.href.includes('sta
                     document.getElementById('patient-info').style.display = 'block';
                 })
                 .catch(error => {
-                    console.error('Error Fetching Patient:', error);
+                    console.error('Error fetching Patient:', error);
                     document.getElementById('status').textContent = 'Failed to fetch patient.';
                 });
 
@@ -34,12 +32,17 @@ if (window.location.href.includes('code=') || window.location.href.includes('sta
             console.error('OAuth2 Ready Error:', error);
             document.getElementById('status').textContent = 'OAuth2 Error';
         });
+
 } else {
-    console.log('Starting Authorization Flow...');
+    console.log('Before Authorize...');
+    console.log('ClientId:', '0be0b3ac-b0ae-40da-97a3-d74a4b66f3d7');
+    console.log('RedirectUri:', 'https://saurabhmindbowser.github.io/SMART-EMBEDED/');
+    console.log('Current URL:', window.location.href);
+    console.log('Starting authorization...');
 
     FHIR.oauth2.authorize({
-        clientId: '0be0b3ac-b0ae-40da-97a3-d74a4b66f3d7',  // <<< your real client_id
+        clientId: '0be0b3ac-b0ae-40da-97a3-d74a4b66f3d7',
         scope: 'launch/patient patient/*.read openid profile offline_access',
-        redirectUri: 'https://saurabhmindbowser.github.io/SMART-EMBEDED/', // <<< Github hosted app
+        redirectUri: 'https://saurabhmindbowser.github.io/SMART-EMBEDED/'
     });
 }
